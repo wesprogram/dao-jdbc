aeb1190527e8915107f5136b0561e7608ae28bec
Wesley Souza <wesleyribeiro@alumni.usp.br>
2022-09-15 14:29:37 -0300

    FindById implementation

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,10 +1,9 @@
 package application;
 
-import java.util.Date;
+import java.util.Scanner;
 
 import model.dao.DaoFactory;
 import model.dao.SellerDao;
-import model.entities.Department;
 import model.entities.Seller;
 
 public class Program {
@@ -12,14 +11,17 @@ public class Program {
 	public static void main(String[] args) {
 		
 		
-		
-		Department obj = new Department(1, "Books");
-		
-		Seller seller = new Seller(21, "bob", "bob@gmail.com", new Date(), 3000.0, obj);
+		Scanner sc = new Scanner(System.in);
 		
 		SellerDao sellerDao = DaoFactory.createSellerDao();
 		
+		System.out.print("Enter the ID: ");
+		
+		Seller seller = sellerDao.findById(sc.nextInt());
+		
 		System.out.println(seller);
+		
+		sc.close();
 
 	}
 
